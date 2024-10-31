03a8fd0d5ec10e0e7ab5eda7632149a68e3cf2da
Grupo-Vitta <matias.artesi@grupoattain.com.ar>
2024-10-31 11:53:31 -0300

    Update __manifest__.py

--- custom_shipping_integration/__manifest__.py
+++ custom_shipping_integration/__manifest__.py
@@ -4,7 +4,7 @@
     'summary': 'Integración de envío personalizado con API externa',
     'description': 'Integra una API externa para calcular y crear envíos en pedidos de venta.',
     'author': 'Tu Nombre o Empresa',
-    'depends': ['sale', 'delivery', 'website_sale'],  # Dependencias necesarias
+    'depends': ['sale', 'delivery'],  # Dependencias necesarias
     'data': [],
     'installable': True,
     'application': False,
