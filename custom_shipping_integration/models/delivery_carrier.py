66d6b87abe16e89a7c3c82bd7aa1b6122ccde93a
Grupo-Vitta <matias.artesi@grupoattain.com.ar>
2024-11-05 14:17:47 -0300

    Update and rename sale_order.py to delivery_carrier.py

--- custom_shipping_integration/models/delivery_carrier.py
+++ custom_shipping_integration/models/delivery_carrier.py
@@ -1,18 +1,19 @@
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 import requests
 import logging
-from odoo import models, fields, api
 
 _logger = logging.getLogger(__name__)
 
-class DeliveryCarrier(models.Model):
+class EntregarShippingCarrier(models.Model):
     _inherit = 'delivery.carrier'
 
     api_token = fields.Char(string="API Token")
-    token_exp = fields.Datetime(string="Token Expiration")
+    token_timestamp = fields.Datetime(string="Token Timestamp")
 
     def _authenticate_api(self):
         url = "https://homologacion.entregarweb.com/api/v1/auth/token"
-        headers = {'Content-type': 'multipart/form-data; boundary=wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'}
+        headers = {'Content-type': 'application/json'}
         payload = {
             'client_api': 'a84e0675aba4322fbebd4ccf164f238deb9ea501b6ac15f16ae4c6aacf590426',
             'client_secret': 'cb4979d3152ee649fb7f94b3ba4ac2625baa803d1ae43e0b7642b6b105b78758'
@@ -22,21 +23,16 @@ class DeliveryCarrier(models.Model):
         if response.status_code == 200:
             token = response.json().get('access_token')
             self.api_token = token
-            self.token_exp = fields.Datetime.now()
+            self.token_timestamp = fields.Datetime.now()
             return token
         else:
-            _logger.error("Failed to authenticate API")
+            _logger.error(f"Failed to authenticate API: {response.status_code} - {response.text}")
             return None
 
-    def is_token_exp(self):
-        if not self.token_exp: #si token_exp no tiene valor o no existe, retorna verdadero (como caducado)
-            return True
-        return fields.Datetime.now() >= self.token_exp
-        
     def _get_valid_token(self):
-        if not self.is_token_exp():
-            return self.api_token
-        return self._authenticate_api()
+        if not self.api_token or not self.token_timestamp or (fields.Datetime.now() - self.token_timestamp).total_seconds() > 6 * 3600:
+            return self._authenticate_api()
+        return self.api_token
 
     def _create_shipping(self, receives, address, location, postal_code, items, email=None, references=None):
         url = "https://homologacion.entregarweb.com/api/v1/shipping/new"
@@ -59,14 +55,14 @@ class DeliveryCarrier(models.Model):
         if response.status_code == 200:
             return response.json()
         else:
-            _logger.error("Failed to create shipping")
+            _logger.error(f"Failed to create shipping: {response.status_code} - {response.text}")
             return None
 
     def action_confirm(self):
-        res = super(DeliveryCarrier, self).action_confirm()
+        res = super(EntregarShippingCarrier, self).action_confirm()
 
         if self.delivery_method_id.name == 'Método de Envío Externo':
-            items = "[\"3,100.0,200.0,30.4\"]"
+            items = [{"quantity": 3, "weight": 100.0, "width": 200.0, "height": 30.4}]
             shipping_data = self._create_shipping(
                 receives=self.partner_id.name,
                 address=self.partner_id.street,
@@ -74,7 +70,7 @@ class DeliveryCarrier(models.Model):
                 postal_code=self.partner_id.zip,
                 items=items,
                 email=self.partner_id.email,
-                references=self.partner_id.street2  # Cambia `street2` si tienes otro campo para referencias
+                references=self.partner_id.street2
             )
             if shipping_data:
                 _logger.info(f"Shipping created successfully: {shipping_data}")
