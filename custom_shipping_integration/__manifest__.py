4d75f769bde20b8f99fdcc88219bb4136ec4171a
Grupo-Vitta <matias.artesi@grupoattain.com.ar>
2024-11-05 14:18:56 -0300

    Update __manifest__.py

--- custom_shipping_integration/__manifest__.py
+++ custom_shipping_integration/__manifest__.py
@@ -1,11 +1,14 @@
 {
-    'name': 'Custom Shipping Integration',
-    'version': '1.0',
-    'summary': 'Integración de envío personalizado con API externa',
-    'description': 'Integra una API externa para calcular y crear envíos en pedidos de venta.',
-    'author': 'Tu Nombre o Empresa',
-    'depends': ['website_sale','stock_delivery'],  # Dependencias necesarias
-    'data': [],
+    'name': 'Entrega Integración Logistica',
+    'version': '17.0',
+    'summary': 'Integración de métodos de envío con Entregar API',
+    'description': 'Módulo personalizado para integrar los métodos de envío de Entregar en Odoo 17.',
+    'author': 'Sebastián Díaz y Agostina Salas',
+    'depends': ['base', 'delivery', 'sale'],
+    'data': [
+        'views/delivery_carrier_views.xml'
+    ],
     'installable': True,
     'application': False,
 }
+
